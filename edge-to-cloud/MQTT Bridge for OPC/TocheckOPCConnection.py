f52a12bf0a403f54a44c3421f330d467b2e0ac58
Partheeban-Krishnan <partheeban.k40@gmail.com>
2025-11-26 15:05:49 +0530

    Delete edge-to-cloud directory

--- edge-to-cloud/MQTT Bridge for OPC/TocheckOPCConnection.py
+++
@@ -1,9 +0,0 @@
-
-from opcua import Client
-
-client = Client("opc.tcp://localhost:55000")
-client.connect()
-node = client.get_node("ns=1;s=Objects/MyFolder/MyStoredVariable")  # Replace with your actual node ID
-datatype = node.get_data_type_as_variant_type()
-print("Expected data type:", datatype)
-client.disconnect()
